b3a2c4241cbbe7adc6d91468c190aa5af0e5f0a8
wzhou <Walter.Zhou@aspiraconnect.com>
2021-06-17 10:46:01 +0800

    add 0001 solution

---
+++ 0001-Two-Sum/Solution0001.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Solution0001 {
+  public static int[] twoSum(int[] nums, int target) {
+    int[] index = new int[2];
+    Map<Integer, Integer> record = new HashMap<>();
+    for (int i = 0; i < nums.length; i ++) {
+      int complement = target - nums[i];
+      Integer oneIndex = record.get(complement);
+      if (oneIndex != null) {
+        index[0] = oneIndex;
+        index[1] = i;
+        break;
+      }
+      record.put(nums[i], i);
+    }
+    return index;
+  }
+
+  public static void main( String[] args ) {
+    int[] nums = {2,7,11,15};
+    int target = 9;
+    int[] result = twoSum( nums, target );
+    System.out.println(Arrays.equals( result, new int[]{ 0, 1 } ));
+
+    nums = new int[]{3, 2, 4};
+    target = 6;
+    result = twoSum( nums, target );
+    System.out.println(Arrays.equals( result, new int[]{ 1, 2 } ));
+
+    nums = new int[]{3, 3};
+    target = 6;
+    result = twoSum( nums, target );
+    System.out.println(Arrays.equals( result, new int[]{ 0, 1 } ));
+
+    nums = new int[]{15, 2, 11, 7};
+    target = 9;
+    result = twoSum( nums, target );
+    System.out.println(Arrays.equals( result, new int[]{ 1, 3 } ));
+  }
+}
