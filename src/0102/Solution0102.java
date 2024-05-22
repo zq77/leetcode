e1999454c1800656e309fdf61525fc3bde330dd7
Walter Zhou <z13903417792@163.com>
2024-05-22 21:43:55 +0800

    add

---
+++ src/0102/Solution0102.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ *
+ */
+public class Solution0102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<List<Integer>> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            final int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                final TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ test/Solution0102Test.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Solution0102Test {
+    private Solution0102 solution;
+    @BeforeEach
+    void setUp() {
+        solution = new Solution0102();
+    }
+    @Test
+    public void test1() {
+        Solution0102.TreeNode right = new Solution0102.TreeNode(20, new Solution0102.TreeNode(15), new Solution0102.TreeNode(7));
+        Solution0102.TreeNode left = new Solution0102.TreeNode(9);
+        Solution0102.TreeNode treeNode = new Solution0102.TreeNode(3, left, right);
+        final List<List<Integer>> lists = solution.levelOrder(treeNode);
+        assertEquals("[[3], [9, 20], [15, 7]]", Arrays.toString(lists.toArray()));
+    }
+    @Test
+    public void test2() {
+
+        Solution0102.TreeNode treeNode = new Solution0102.TreeNode(1);
+        final List<List<Integer>> lists = solution.levelOrder(treeNode);
+        assertEquals("[[1]]", Arrays.toString(lists.toArray()));
+    }
+
+    @Test
+    public void test3() {
+        final List<List<Integer>> lists = solution.levelOrder(null);
+        assertEquals("[]", Arrays.toString(lists.toArray()));
+    }
+}
